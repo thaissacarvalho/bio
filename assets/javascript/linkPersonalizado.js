611d1f76a60780d20e545aca9d4421b8c8ea82c5
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2023-05-09 12:03:21 -0300

    atualizando HTML, responsividade e JS.

--- assets/javascript/linkPersonalizado.js
+++ assets/javascript/linkPersonalizado.js
@@ -2,10 +2,10 @@ const link = document.getElementById('link');
 var less = false;
 
 link.addEventListener('click', function(){
-  this.innerHTML = (less = !less) ? `<img src="assets/images/Site.svg" alt="Icone de globo que te levará ao meu site." 
-  class="botao__imagem"> 
-  <p class="botao__texto">Site</p>` 
+  this.innerHTML = (less = !less) ? `<img src="assets/images/Site.svg" 
+  alt="Icone de globo que te levará ao meu site. O site ainda não está disponivel." class="botao__imagem">
+  <span class="botao__texto">Site</span>` 
   : `<img src="assets/images/Site.svg" alt="Icone de globo que te levará ao meu site."
-    class="botao__imagem">
-    <p class="botao__texto">Disponivel em breve</p>`;
+  class="botao__imagem">
+  <p class="botao__texto">Disponivel em breve</p>`;
 });
