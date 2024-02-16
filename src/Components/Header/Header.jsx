46374b79d9d86f8bf4b772044384fd8346cb0bee
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2024-02-16 19:29:46 -0300

    Changed file to reflect my situation in the job market

---
+++ src/Components/Header/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header className="justify-self-start	self-start gap-3">
+        <h3 className="text-style-purple font-medium relative top-3 left-4 hover:text-green-400">#OpenToWork</h3>
+    </header>
+  )
+}
