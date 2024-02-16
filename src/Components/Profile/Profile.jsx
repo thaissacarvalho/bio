da9b799cdbe52c28a5c14d2678db351b429fe3c3
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2024-02-16 19:39:19 -0300

    Created file to picture and two texts with my name e profession

---
+++ src/Components/Profile/Profile.jsx
@@ -0,0 +1,12 @@
+export default function Profile() {
+    return (
+        <div className="flex flex-col justify-center items-center">
+            <img src="/eu-sorrindo.svg" alt="My memoji photo. I'm smile and I make a 2 with hands" className="w-36 h-36" />
+
+            <span className="flex flex-col items-center">
+                <h2 className="text-lg font-bold">Thaissa Carvalho</h2>
+                <h3 className="text-sm font-bold text-style-purple">Desenvolvedor Full Stack</h3>
+            </span>
+        </div>
+    )
+}
