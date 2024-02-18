243948ba648907d3ae6c30f69625b4a1c174816d
Thaissa Carvalho <thaissa.dos2003@gmail.com>
2024-02-18 15:08:14 -0300

    updated links curriculum/resume

--- src/Components/Main/Main.jsx
+++ src/Components/Main/Main.jsx
@@ -8,8 +8,8 @@ export default function Main() {
         <Profile />
         <div className="flex flex-col gap-4 justify-center items-center">
           <Button linkName="https://thaissacarvalho.vercel.app/" textName="ACESSE O MEU WEBSITE" />
-          <Button linkName="https://1drv.ms/w/s!AvyeC1pPN5L-jTL7iZ-Lau5agES8?e=JdAreR" textName="CURRICULO FRONT END" />
-          <Button linkName="https://1drv.ms/w/s!AvyeC1pPN5L-jTRRA0NnErBIlwuf?e=J6LiGT" textName="CURRICULO FULL STACK" />
+          <Button linkName="https://1drv.ms/w/s!AvyeC1pPN5L-jTLQiTfbL6M1lmhN?e=WcFND5" textName="CURRICULO FRONT END" />
+          <Button linkName="https://1drv.ms/w/s!AvyeC1pPN5L-jTTK5wHwFPRC8GjO" textName="CURRICULO FULL STACK" />
         </div>
         <SocialMedia />
       </main>
